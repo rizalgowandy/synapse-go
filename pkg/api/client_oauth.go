f01c53e10a20368eb04a6cde731c3c61a3a64f86
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2023-03-08 19:36:06 +0700

    Add GenerateRefreshToken

--- pkg/api/client_oauth.go
+++ pkg/api/client_oauth.go
@@ -34,3 +34,32 @@ func (c *Client) CreateOAuthKey(ctx context.Context, req *entity.CreateOAuthKeyR
 
 	return &content, nil
 }
+
+func (c *Client) GenerateRefreshToken(ctx context.Context, req *entity.GenerateRefreshTokenReq) (*entity.GenerateRefreshTokenResp, error) {
+	url := "/v3.1/oauth/{user_id}/login"
+
+	var (
+		content    entity.GenerateRefreshTokenResp
+		contentErr entity.ErrResp
+	)
+
+	_, err := c.client.R().
+		SetContext(ctx).
+		SetPathParam("user_id", req.UserID).
+		SetHeaders(c.UserIPHeader(req.UserIPAddress)).
+		SetHeaders(c.UserHeader("", req.UserID)).
+		SetHeader("Content-Type", "application/json").
+		SetBody(req).
+		SetResult(&content).
+		SetError(&contentErr).
+		Post(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if contentErr.Valid() {
+		return nil, contentErr
+	}
+
+	return &content, nil
+}
--- pkg/entity/oauth.go
+++ pkg/entity/oauth.go
@@ -3,13 +3,12 @@ package entity
 import "github.com/rizalgowandy/synapse-go/pkg/enum"
 
 type CreateOAuthKeyReq struct {
-	UserID                string       `json:"-"`
-	UserIPAddress         string       `json:"-"`
-	UserDeviceFingerprint string       `json:"-"`
-	ValidationPin         string       `json:"validation_pin"`
-	PhoneNumber           string       `json:"phone_number"`
-	Scope                 []enum.Scope `json:"scope"`
-	RefreshToken          string       `json:"refresh_token"`
+	UserID        string       `json:"-"`
+	UserIPAddress string       `json:"-"`
+	ValidationPin string       `json:"validation_pin"`
+	PhoneNumber   string       `json:"phone_number"`
+	Scope         []enum.Scope `json:"scope"`
+	RefreshToken  string       `json:"refresh_token"`
 }
 
 type CreateOAuthKeyResp struct {
@@ -22,4 +21,24 @@ type CreateOAuthKeyResp struct {
 	RefreshToken     string       `json:"refresh_token"`
 	Scope            []enum.Scope `json:"scope"`
 	UserID           string       `json:"user_id"`
+}
+
+type GenerateRefreshTokenReq struct {
+	UserID        string `json:"-"`
+	UserIPAddress string `json:"-"`
+	Email         string `json:"email"`
+	Password      string `json:"password"`
+}
+
+type GenerateRefreshTokenResp struct {
+	ID           string       `json:"_id"`
+	V            int          `json:"_v"`
+	ChangeIn     int          `json:"change_in"`
+	ClientID     string       `json:"client_id"`
+	ClientName   string       `json:"client_name"`
+	FpRegistered bool         `json:"fp_registered"`
+	IsActive     bool         `json:"is_active"`
+	RefreshToken string       `json:"refresh_token"`
+	Scope        []enum.Scope `json:"scope"`
+	UserID       string       `json:"user_id"`
 }
