9cf79a1dae612db893f798a040bf2ac89832a201
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2023-07-12 11:18:24 +0700

    Replace Make with Task

--- .github/pre-commit.sh
+++ .github/pre-commit.sh
@@ -10,26 +10,26 @@
 # List all nonformatted files
 files=$(git diff --cached --name-only --diff-filter=ACM | grep '\.go$')
 
-# Some files are not formatted with gofmt. Print message.
-nonformatted=$(gofmt -l $files)
+# Some files are not formatted with goimports. Print message.
+nonformatted=$(goimports -l $files)
 if [ "$nonformatted" ]; then
-  echo >&2 "Go files must be formatted with gofmt. Running:"
+  echo >&2 "Go files must be formatted with goimports. Running:"
   for fn in $nonformatted; do
-    echo >&2 "  gofmt -w $PWD/$fn"
-    gofmt -w "$PWD/$fn"
+    echo >&2 "  goimports -w $PWD/$fn"
+    goimports -w "$PWD/$fn"
     git add "$PWD/$fn"
   done
   printf "\n"
 fi
 
 # Run linter.
-make linter || exit 1
+task analysis || exit 1
 
 # Build binaries to ensure program can be built
-make build || exit 1
+task build || exit 1
 
 # Run test.
-make test || exit 1
+task unit_tests || exit 1
 
 # Clean up unused dependency.
 go mod tidy
